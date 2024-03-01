b61827951a12517463adf59e6b3b1dce141d000d
mateuslph <mlptche@gmail.com>
2024-02-29 21:31:14 -0300

    inicia estilizacao dinamica

--- src/App.js
+++ src/App.js
@@ -5,6 +5,44 @@ import Time from './componentes/Time';
 
 function App() {
 
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '#f0f8e2'
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#ed6b69',
+      corSecundaria: '#fde7e8'
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#db6e8f',
+      corSecundaria: '#fae9f5'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeedf'
+    }
+  ]
+
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicioado = (colaborador) => {
@@ -16,8 +54,8 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicioado(colaborador)}/>
-      <Time nome="Programação"/>
-      <Time nome="Front-End"/>
+      
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     </div>
   );
 }
