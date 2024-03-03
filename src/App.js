5c7b49b38d9f7fdd4755e903bd3dbc561ea3f5a5
mateuslph <mlptche@gmail.com>
2024-03-03 18:59:04 -0300

    tag para limpar times

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicioado = (colaborador) => {
-    console.log(colaborador)
+    //console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
   }
 
